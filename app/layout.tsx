7abb65b53fc8a64c6cbae68a8d27399f87c6e7ec
Olaoluwa J. Taiwo <taiwo.olaoluwaj@gmail.com>
2026-08-09 06:24:17 +0100

    Load sitewide alignment guard

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import "./expansion.css";
 import "./collaboration.css";
 import "./polish.css";
 import "./page-system.css";
+import "./alignment.css";
 
 const inter=Inter({subsets:["latin"],variable:"--font-inter",display:"swap"});
 const space=Space_Grotesk({subsets:["latin"],variable:"--font-space",display:"swap"});

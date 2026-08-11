719c43e16cd85b2508cc377bb23e631575e8502d
Olaoluwa J. Taiwo <taiwo.olaoluwaj@gmail.com>
2026-08-11 01:10:05 +0100

    Wrap member routes in authenticated app shell

---
+++ app/member/layout.tsx
@@ -0,0 +1,5 @@
+import MemberAppShell from '@/components/MemberAppShell';
+
+export default function MemberLayout({children}:{children:React.ReactNode}){
+  return <MemberAppShell>{children}</MemberAppShell>;
+}

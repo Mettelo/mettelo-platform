4f4f1600b94d6f32542c3b8f25382a61f49b2292
Olaoluwa J. Taiwo <taiwo.olaoluwaj@gmail.com>
2026-08-16 00:24:35 +0100

    Secure Phase 2 application event trigger function

---
+++ supabase/migrations/20260816003500_secure_project_application_event_trigger.sql
@@ -0,0 +1,3 @@
+revoke execute on function public.record_project_application_event() from public;
+revoke execute on function public.record_project_application_event() from anon;
+revoke execute on function public.record_project_application_event() from authenticated;
